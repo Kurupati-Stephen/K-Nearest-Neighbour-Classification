b08e3c08460891fc6c13f19bf5e653151cdb6238
Kurupati-Stephen <kurupatistephen@gmail.com>
2025-06-04 15:29:54 +0530

    Add files via upload

---
+++ KNNclassification.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "70a63a08-3346-492f-b5e3-165223c61d85",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
